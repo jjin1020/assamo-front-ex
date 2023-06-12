be59a976e50bbd35e922b4b8732966ac1fc3d460
정진룡 <jinlyong1020@gmail.com>
2023-06-13 04:26:33 +0900

    first commit

---
+++ src/components/home/hero.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Animation from "../animation";
+
+export default function Hero() {
+
+    return (
+        <>
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">안녕하세요. 아싸모입니다.
+                    <br className="hidden lg:inline-block" />오늘도 우유를 찾아주셔서 감사합니다.
+                </h1>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <Animation></Animation>
+            </div>
+        </>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,5 +15,7 @@ module.exports = {
       },
     },
   },
+  darkMode: 'class',
+  mode: 'jit',
   plugins: [],
 }
