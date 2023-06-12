913708eb8af595ab5390f789b5c28be7e3f17f89
정진룡 <jinlyong1020@gmail.com>
2023-06-13 08:04:27 +0900

    ## proxy 설정 및 rxjs 설치 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+     return [
+              {
+                 source: "/board:path*",
+                 destination: "http://localhost:8081/:path*"
+              }
+     ]
+  }
 }
 
 module.exports = nextConfig
