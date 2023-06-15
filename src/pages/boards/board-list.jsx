dc780c4db9c73cf65f80eded9425a03b5a143dcf
정진룡 <jinlyong1020@gmail.com>
2023-06-16 07:13:37 +0900

    ## 신규, 목록 버튼 추가

--- src/pages/boards/board-list.jsx
+++ src/pages/boards/board-list.jsx
@@ -5,8 +5,11 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import BbsList from '@/components/bbs/bbs-list';
 import { ajax } from 'rxjs/ajax'
+import { useRouter } from 'next/router';
 
 export default function BoardList() {
+    const router = useRouter();
+
     const [boardList, setBoardList] = useState([]);
 
     useEffect(() => {
@@ -26,6 +29,13 @@ export default function BoardList() {
         }
     }, []);
 
+    const handleReqBtn = (event) => {
+        event.preventDefault();
+
+        router.push('/boards/board');
+
+    }
+
     return (
         <>
             <Layout>
@@ -36,6 +46,7 @@ export default function BoardList() {
                 </Head>
                 <div className="px-4 py-12 sm:px-6 lg:px-8 min-h-screen">
                     <div className="mx-auto max-w-4xl">
+                        <button type="button" onClick={handleReqBtn} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">신규</button>
                         <ul role="list" className="divide-y divide-gray-100">
                             {boardList && <BbsList bbsList={boardList}/>}
                         </ul>
--- src/pages/boards/board.jsx
+++ src/pages/boards/board.jsx
@@ -109,6 +109,11 @@ export default function Board() {
             router.push('/boards/board-list');
         });
     }
+    
+    const onMoveList = (event) => {
+        event.preventDefault();
+        router.push('/boards/board-list');
+    }
 
     return (
         <>
@@ -263,6 +268,7 @@ export default function Board() {
                             <div className="mt-6 flex items-center justify-end gap-x-6">
                                 <button type="button" className="text-sm font-semibold leading-6 text-gray-900" onClick={deleteClick}>삭제</button>
                                 <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">저장</button>
+                                <button type="button" onClick={onMoveList} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">목록</button>
                             </div>
                         </form>
                     </div>
